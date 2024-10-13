d19cf49fc45e35c100d4f81ff2962648818445cf
Emre Yildirim <temreyildirim8@gmail.com>
2024-10-13 20:50:41 +0300

    fix: align pair selections right

--- src/components/PairSelector/PairSelector.js
+++ src/components/PairSelector/PairSelector.js
@@ -5,13 +5,32 @@ function PairSelector({ selectedPair, onSelect }) {
   return (
     <Box sx={{ minWidth: 120 }}>
       <FormControl fullWidth>
-        <InputLabel id="pair-selector-label">Select Pair</InputLabel>
+        <InputLabel id="pair-selector-label" color='primary'>Select Pair</InputLabel>
         <Select
           labelId="pair-selector-label"
           id="pair-selector"
           value={selectedPair}
           label="Select Pair"
           onChange={(e) => onSelect(e.target.value)}
+          MenuProps={{
+            anchorOrigin: {
+              vertical: 'bottom',
+              horizontal: 'right',
+            },
+            transformOrigin: {
+              vertical: 'top',
+              horizontal: 'right',
+            },
+            PaperProps: {
+              sx: {
+                '& .MuiMenuItem-root': {
+                  justifyContent: 'flex-end',
+                  marginRight: '10px',
+                  paddingLeft: '10px'
+                }
+              }
+            }
+          }}
         >
           <MenuItem value="BTCUSDT">BTC/USDT</MenuItem>
           <MenuItem value="ETHUSDT">ETH/USDT</MenuItem>
